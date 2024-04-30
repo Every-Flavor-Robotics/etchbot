aaece0547bdfd027ce384d1cdf836f43921a5b78
Swapnil Pande <swapnil.pande98@gmail.com>
2024-04-30 14:18:26 -0400

    Working version of gcode server

--- gcode_server.py
+++ gcode_server.py
@@ -3,20 +3,46 @@ import socket
 host = ""  # Listen on all interfaces
 port = 5000
 
-gcode_drawing = "../gcode-optimizer/test.gcode"
-gcode_calibration = "test_circle.gcode"
+# gcode_drawing = "pipeline_output/couch_hand_drawing/filtered.gcode"
+# gcode_calibration = "pipeline_output/test_circle.gcode"
 
-# Read in G-code file test/final.gcode
-with open(gcode_drawing) as f:
-    gcode_lines = f.readlines()
-    gcode_lines.append("G28\n")
+# gcode_lines = []
+# with open(gcode_calibration) as f:
+#     gcode_lines = f.readlines()
+#     gcode_lines.append("G28\n")
 
-    # start_line = 0
-    # end_line = 10
 
-    # import ipdb
+title_cards = [
+    "pipeline_output/title_10/filtered.gcode",
+    "pipeline_output/title_09/filtered.gcode",
+    "pipeline_output/title_08/filtered.gcode",
+    "pipeline_output/title_07/filtered.gcode",
+    "pipeline_output/title_06/filtered.gcode",
+    "pipeline_output/title_05/filtered.gcode",
+    "pipeline_output/mogodemo/filtered.gcode",
+    "pipeline_output/pikachu/filtered.gcode",
+    "pipeline_output/couch_hand_drawing/filtered.gcode",
+    "pipeline_output/this_is_fine/filtered.gcode",
+    "pipeline_output/doggo/filtered.gcode",
+]
 
-    # ipdb.set_trace()
+# Read all in and chain them together
+gcode_lines = []
+for title_card in title_cards:
+    print("Reading title card:", title_card)
+    with open(title_card) as f:
+        gcode_lines.extend(f.readlines())
+        gcode_lines.append("G28\n")
+
+# Remove all comments
+# for i in range(len(gcode_lines)):
+#     # Look for a semicolon in line
+#     comment_index = gcode_lines[i].find(";")
+#     if comment_index != -1:
+#         gcode_lines[i] = gcode_lines[i][:comment_index] + "\n"
+
+# # Remove all empty lines
+# gcode_lines = [line for line in gcode_lines if line.strip() != ""]
 
 
 def main():
@@ -71,3 +97,88 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+# import socketserver
+# import time
+# import threading
+
+# kill_signal = False
+# kill_signal_served = True
+
+
+# class GCodeRequestHandler(socketserver.StreamRequestHandler):
+#     start_line = 0
+
+#     def handle(self):
+#         global kill_signal, kill_signal_served
+
+#         print("Connected by", self.client_address)
+
+#         # Signal to all previous processes to stop
+#         kill_signal = True
+#         while not kill_signal_served:
+#             time.sleep(1)
+#         # Reset the kill signal
+#         kill_signal_served = False
+#         kill_signal = False
+
+#         last_heartbeat = time.time()
+#         # Start the heartbeat thread
+#         threading.Thread(target=self.send_heartbeat, args=(1,)).start()
+
+#         while not kill_signal:
+#             print("Waiting for data...")
+#             data = self.request.recv(1024).strip().decode()
+#             print(type(data))
+#             print("Received data")
+#             print("------ data start ------")
+#             print(data)
+#             print("------ data end --------")
+#             if data.startswith("GET /gcode?lines="):
+#                 try:
+#                     num_lines_requested = int(data.split("=")[1].split(" ")[0])
+#                 except (ValueError, IndexError):
+#                     self.wfile.write(b"Invalid request format.\n")
+#                     continue
+#                 # Send the G-code lines
+#                 print(
+#                     len(
+#                         gcode_lines[
+#                             self.start_line : self.start_line + num_lines_requested
+#                         ]
+#                     )
+#                 )
+#                 gcode_block = "".join(
+#                     gcode_lines[self.start_line : self.start_line + num_lines_requested]
+#                 )
+#                 self.start_line += num_lines_requested
+#                 if self.start_line >= len(gcode_lines):
+#                     self.start_line = 0
+#                 print("Sending response")
+#                 print(gcode_block)
+#                 self.wfile.write(gcode_block.encode())
+#                 print("Response sent")
+
+#             # if time.time() - last_heartbeat > 1:
+#             #     self.send_heartbeat(1)
+#             #     last_heartbeat = time.time()
+#         print("Received a kill signal... Closing connection.")
+
+#     def send_heartbeat(self, interval):
+
+#         last_heartbeat = time.time()
+#         while True:
+#             if time.time() - last_heartbeat > interval:
+#                 # Heartbeat is 0x00 ASCII
+#                 print("Sending heartbeat...")
+#                 heartbeat_message = "\x01"
+#                 self.request.sendall(heartbeat_message.encode())
+#                 last_heartbeat = time.time()
+
+
+# if __name__ == "__main__":
+#     host, port = "0.0.0.0", 5000  # Your host and port here
+#     with socketserver.TCPServer((host, port), GCodeRequestHandler) as server:
+#         print("Server listening...")
+#         server.serve_forever()
