9ba84e08797927bdeaf60fb06d1901e5a5c16bbb
Swapnil Pande <swapnil.pande98@gmail.com>
2024-08-12 17:12:01 -0700

    Cleaned up latest server code

--- etch-a-sketch-server/etch_a_sketch_server.py
+++ etch-a-sketch-server/etch_a_sketch_server.py
@@ -60,8 +60,16 @@ def upload_file():
         return jsonify({"error": "Failed to process file"}), 500
 
 
-# Create a function that constantly checks UPLOAD DIR for new files
 def process_files():
+    """Function that constantly checks UPLOAD DIR for new files and processes them.
+
+    Supported file types:
+    - Images: .jpg, .jpeg, .png, .svg
+    - Videos: .mp4, .avi, .mov
+    - GCode: .gcode
+    - Optimized GCode: .optgcode <- bypasses all processing and is directly sent to the GCode server
+
+    """
     print("Processing thread started.")
     while True:
         for file_path in UPLOAD_DIR.iterdir():
@@ -93,9 +101,11 @@ def process_files():
 
 
 ### EtchBot API ###
+# This API is used to communicate with the EtchBots
 
 
 def gcode_server():
+    """Function to start the GCode server."""
     print("GCode server started.")
     command = "python gcode_server.py --gcode_dir gcode_outputs"
 
@@ -108,18 +118,42 @@ def gcode_server():
 
 @app.route("/update_state", methods=["POST"])
 def update_state():
+    """API endpoint to update the state of an EtchBot.
+
+    The request should contain a JSON object with the following fields:
+    - name: The name of the EtchBot
+    - current_state: The state to transition to
+    - timestamp: The timestamp of the state update
+
+    Example request:
+    {
+        "name": "EtchBot1",
+        "current_state": "Idle",
+        "timestamp": "2021-08-15T12:00:00Z"
+    }
+
+    """
+
     try:
         data = request.get_json()
-        if not data or "name" not in data or "current_state" not in data:
+        # Check if the request contains the required fields
+        if (
+            not data
+            or "name" not in data
+            or "current_state" not in data
+            or "timestamp" not in data
+        ):
             return jsonify({"error": "Invalid data"}), 400
 
         name = data["name"]
         current_state = data["current_state"]
         timestamp = data.get("timestamp", datetime.utcnow().isoformat() + "Z")
 
+        # Check if the EtchBot instance already exists, if not create a new one
         if name not in etchbots:
             etchbots[name] = EtchBot(name)
 
+        # Get the EtchBot instance from the dictionary
         etchbot = etchbots[name]
 
         # Check if the current state is valid and trigger the corresponding transition
@@ -140,7 +174,7 @@ def update_state():
 
 
 if __name__ == "__main__":
-    # Confirm that the output directory exists
+    # Confirm that the necessary directories exist
     if not UPLOAD_DIR.exists():
         UPLOAD_DIR.mkdir()
 
@@ -154,124 +188,120 @@ if __name__ == "__main__":
     processing_thread = threading.Thread(target=process_files)
     processing_thread.start()
 
-    # Start the GCode server thread
-    # gcode_thread = threading.Thread(target=gcode_server)
-    # gcode_thread.start()
-
     run_gcode_server(OUTPUT_DIR, run_flask=False)
 
     app.run("0.0.0.0", port=5010, use_reloader=False, debug=False)
 
 
-from flask import Flask, request, jsonify
-from flask_cors import CORS
-from pathlib import Path
-import threading
-import time
-import shutil
-import subprocess
-from gcode_server import (
-    gcode_blueprint,
-    run_gcode_server,
-)
+# from flask import Flask, request, jsonify
+# from flask_cors import CORS
+# from pathlib import Path
+# import threading
+# import time
+# import shutil
+# import subprocess
+# from gcode_server import (
+#     gcode_blueprint,
+#     run_gcode_server,
+# )
 
-UPLOAD_DIR = Path("uploads")
-PROCESSING_DIR = Path("processing")
-OUTPUT_DIR = Path("gcode_outputs")
-SUPPORTED_IMAGE_TYPES = [".jpg", ".jpeg", ".png", ".svg"]
-SUPPORTED_VIDEO_TYPES = [".mp4", ".avi", ".mov"]
-SUPPORTED_GCODE_TYPES = [".gcode"]
-SUPPORTED_FILE_TYPES = (
-    SUPPORTED_IMAGE_TYPES + SUPPORTED_VIDEO_TYPES + SUPPORTED_GCODE_TYPES
-)
+# UPLOAD_DIR = Path("uploads")
+# PROCESSING_DIR = Path("processing")
+# OUTPUT_DIR = Path("gcode_outputs")
+# SUPPORTED_IMAGE_TYPES = [".jpg", ".jpeg", ".png", ".svg"]
+# SUPPORTED_VIDEO_TYPES = [".mp4", ".avi", ".mov"]
+# SUPPORTED_GCODE_TYPES = [".gcode"]
+# SUPPORTED_FILE_TYPES = (
+#     SUPPORTED_IMAGE_TYPES + SUPPORTED_VIDEO_TYPES + SUPPORTED_GCODE_TYPES
+# )
 
-app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes
-app.register_blueprint(gcode_blueprint)
-
-
-@app.route("/upload", methods=["POST"])
-def upload_file():
-    try:
-        print("Receiving file...")
-        file = request.files["file"]
-        if not file:
-            return jsonify({"error": "No file provided"}), 400
-
-        file_extension = Path(file.filename).suffix.lower()
-
-        if file_extension not in SUPPORTED_FILE_TYPES:
-            return jsonify({"error": "Unsupported file type"}), 400
-
-        save_path = UPLOAD_DIR / f"{time.strftime('%m%d%H%M%S')}{file_extension}"
-        file.save(save_path)
-
-        return jsonify({"message": "File processing started."}), 200
-    except Exception as e:
-        print(f"Error during file upload: {e}")
-        return jsonify({"error": "Failed to process file"}), 500
+# app = Flask(__name__)
+# CORS(app)  # Enable CORS for all routes
+# app.register_blueprint(gcode_blueprint)
 
 
-# Create a function that constantly checks UPLOAD DIR for new files
-def process_files():
-    print("Processing thread started.")
-    while True:
-        for file_path in UPLOAD_DIR.iterdir():
-            try:
-                print("Processing file:", file_path)
-                file_extension = file_path.suffix.lower()
-                if file_extension in SUPPORTED_IMAGE_TYPES:
-                    processing_dir = PROCESSING_DIR / file_path.stem
-                    run_pipeline(file_path, processing_dir, copy=False)
+# @app.route("/upload", methods=["POST"])
+# def upload_file():
+#     try:
+#         print("Receiving file...")
+#         file = request.files["file"]
+#         if not file:
+#             return jsonify({"error": "No file provided"}), 400
 
-                    # Copy gcode to output directory, named input file name.gcode
-                    gcode_path = processing_dir / "output.gcode"
-                    output_path = OUTPUT_DIR / f"{file_path.stem}.gcode"
-                    shutil.copy(gcode_path, output_path)
-                    file_path.unlink()  # Remove the processed image
-                elif (
-                    file_extension in SUPPORTED_VIDEO_TYPES
-                    or file_extension in SUPPORTED_GCODE_TYPES
-                ):
-                    # For video and GCode files, just move them to the processing directory
-                    processing_dir = PROCESSING_DIR / file_path.name
-                    shutil.move(str(file_path), processing_dir)
-            except Exception as e:
-                print(f"Error during file processing: {e}")
+#         file_extension = Path(file.filename).suffix.lower()
 
-        time.sleep(1)
+#         if file_extension not in SUPPORTED_FILE_TYPES:
+#             return jsonify({"error": "Unsupported file type"}), 400
+
+#         save_path = UPLOAD_DIR / f"{time.strftime('%m%d%H%M%S')}{file_extension}"
+#         file.save(save_path)
+
+#         return jsonify({"message": "File processing started."}), 200
+#     except Exception as e:
+#         print(f"Error during file upload: {e}")
+#         return jsonify({"error": "Failed to process file"}), 500
 
 
-def gcode_server():
-    print("GCode server started.")
-    command = "python gcode_server.py --gcode_dir gcode_outputs"
+# # Create a function that constantly checks UPLOAD DIR for new files
+# def process_files():
+#     print("Processing thread started.")
+#     while True:
+#         for file_path in UPLOAD_DIR.iterdir():
+#             try:
+#                 print("Processing file:", file_path)
+#                 file_extension = file_path.suffix.lower()
+#                 if file_extension in SUPPORTED_IMAGE_TYPES:
+#                     processing_dir = PROCESSING_DIR / file_path.stem
+#                     run_pipeline(file_path, processing_dir, copy=False)
 
-    subprocess.run(command, shell=True)
+#                     # Copy gcode to output directory, named input file name.gcode
+#                     gcode_path = processing_dir / "output.gcode"
+#                     output_path = OUTPUT_DIR / f"{file_path.stem}.gcode"
+#                     shutil.copy(gcode_path, output_path)
+#                     file_path.unlink()  # Remove the processed image
+#                 elif (
+#                     file_extension in SUPPORTED_VIDEO_TYPES
+#                     or file_extension in SUPPORTED_GCODE_TYPES
+#                 ):
+#                     # For video and GCode files, just move them to the processing directory
+#                     processing_dir = PROCESSING_DIR / file_path.name
+#                     shutil.move(str(file_path), processing_dir)
+#             except Exception as e:
+#                 print(f"Error during file processing: {e}")
 
-    # Wait for the subprocess to finish
-    print("GCode server finished.")
-    time.sleep(1)
+#         time.sleep(1)
 
 
-if __name__ == "__main__":
-    # Confirm that the output directory exists
-    if not UPLOAD_DIR.exists():
-        UPLOAD_DIR.mkdir()
+# def gcode_server():
+#     print("GCode server started.")
+#     command = "python gcode_server.py --gcode_dir gcode_outputs"
 
-    if not PROCESSING_DIR.exists():
-        PROCESSING_DIR.mkdir()
+#     subprocess.run(command, shell=True)
 
-    if not OUTPUT_DIR.exists():
-        OUTPUT_DIR.mkdir()
+#     # Wait for the subprocess to finish
+#     print("GCode server finished.")
+#     time.sleep(1)
 
-    # Start the processing thread
-    processing_thread = threading.Thread(target=process_files)
-    processing_thread.start()
 
-    # Start the GCode server thread
-    # gcode_thread = threading.Thread(target=gcode_server)
-    # gcode_thread.start()
+# if __name__ == "__main__":
+#     # Confirm that the output directory exists
+#     if not UPLOAD_DIR.exists():
+#         UPLOAD_DIR.mkdir()
 
-    run_gcode_server(OUTPUT_DIR, run_flask=False)
+#     if not PROCESSING_DIR.exists():
+#         PROCESSING_DIR.mkdir()
 
-    app.run("0.0.0.0", port=5010, use_reloader=False, debug=False)
+#     if not OUTPUT_DIR.exists():
+#         OUTPUT_DIR.mkdir()
+
+#     # Start the processing thread
+#     processing_thread = threading.Thread(target=process_files)
+#     processing_thread.start()
+
+#     # Start the GCode server thread
+#     # gcode_thread = threading.Thread(target=gcode_server)
+#     # gcode_thread.start()
+
+#     run_gcode_server(OUTPUT_DIR, run_flask=False)
+
+#     app.run("0.0.0.0", port=5010, use_reloader=False, debug=False)
