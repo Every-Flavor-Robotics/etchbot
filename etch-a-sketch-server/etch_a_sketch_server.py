946b27e11f868c51d6e0588d464e91efdb9e4238
Swapnil Pande <swapnil.pande98@gmail.com>
2024-08-12 17:24:23 -0700

    Removed unneeded functions, imports, old commented code

--- etch-a-sketch-server/etch_a_sketch_server.py
+++ etch-a-sketch-server/etch_a_sketch_server.py
@@ -2,11 +2,9 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from pathlib import Path
 import datetime
-import io
 import threading
 import time
 import shutil
-import subprocess
 from etch_a_sketch_cli import run_pipeline
 from gcode_server import (
     gcode_blueprint,
@@ -34,6 +32,8 @@ etchbots = {}
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
+# Register the GCode blueprint, so that the GCode server can be started
+# As a part of the main Flask app
 app.register_blueprint(gcode_blueprint)
 
 
@@ -102,18 +102,6 @@ def process_files():
 
 ### EtchBot API ###
 # This API is used to communicate with the EtchBots
-
-
-def gcode_server():
-    """Function to start the GCode server."""
-    print("GCode server started.")
-    command = "python gcode_server.py --gcode_dir gcode_outputs"
-
-    subprocess.run(command, shell=True)
-
-    # Wait for the subprocess to finish
-    print("GCode server finished.")
-    time.sleep(1)
 
 
 @app.route("/update_state", methods=["POST"])
@@ -190,118 +178,8 @@ if __name__ == "__main__":
 
     run_gcode_server(OUTPUT_DIR, run_flask=False)
 
+    print("Registered Endpoints:")
+    for rule in app.url_map.iter_rules():
+        print(f"{rule.endpoint}: {rule.rule}")
+
     app.run("0.0.0.0", port=5010, use_reloader=False, debug=False)
-
-
-# from flask import Flask, request, jsonify
-# from flask_cors import CORS
-# from pathlib import Path
-# import threading
-# import time
-# import shutil
-# import subprocess
-# from gcode_server import (
-#     gcode_blueprint,
-#     run_gcode_server,
-# )
-
-# UPLOAD_DIR = Path("uploads")
-# PROCESSING_DIR = Path("processing")
-# OUTPUT_DIR = Path("gcode_outputs")
-# SUPPORTED_IMAGE_TYPES = [".jpg", ".jpeg", ".png", ".svg"]
-# SUPPORTED_VIDEO_TYPES = [".mp4", ".avi", ".mov"]
-# SUPPORTED_GCODE_TYPES = [".gcode"]
-# SUPPORTED_FILE_TYPES = (
-#     SUPPORTED_IMAGE_TYPES + SUPPORTED_VIDEO_TYPES + SUPPORTED_GCODE_TYPES
-# )
-
-# app = Flask(__name__)
-# CORS(app)  # Enable CORS for all routes
-# app.register_blueprint(gcode_blueprint)
-
-
-# @app.route("/upload", methods=["POST"])
-# def upload_file():
-#     try:
-#         print("Receiving file...")
-#         file = request.files["file"]
-#         if not file:
-#             return jsonify({"error": "No file provided"}), 400
-
-#         file_extension = Path(file.filename).suffix.lower()
-
-#         if file_extension not in SUPPORTED_FILE_TYPES:
-#             return jsonify({"error": "Unsupported file type"}), 400
-
-#         save_path = UPLOAD_DIR / f"{time.strftime('%m%d%H%M%S')}{file_extension}"
-#         file.save(save_path)
-
-#         return jsonify({"message": "File processing started."}), 200
-#     except Exception as e:
-#         print(f"Error during file upload: {e}")
-#         return jsonify({"error": "Failed to process file"}), 500
-
-
-# # Create a function that constantly checks UPLOAD DIR for new files
-# def process_files():
-#     print("Processing thread started.")
-#     while True:
-#         for file_path in UPLOAD_DIR.iterdir():
-#             try:
-#                 print("Processing file:", file_path)
-#                 file_extension = file_path.suffix.lower()
-#                 if file_extension in SUPPORTED_IMAGE_TYPES:
-#                     processing_dir = PROCESSING_DIR / file_path.stem
-#                     run_pipeline(file_path, processing_dir, copy=False)
-
-#                     # Copy gcode to output directory, named input file name.gcode
-#                     gcode_path = processing_dir / "output.gcode"
-#                     output_path = OUTPUT_DIR / f"{file_path.stem}.gcode"
-#                     shutil.copy(gcode_path, output_path)
-#                     file_path.unlink()  # Remove the processed image
-#                 elif (
-#                     file_extension in SUPPORTED_VIDEO_TYPES
-#                     or file_extension in SUPPORTED_GCODE_TYPES
-#                 ):
-#                     # For video and GCode files, just move them to the processing directory
-#                     processing_dir = PROCESSING_DIR / file_path.name
-#                     shutil.move(str(file_path), processing_dir)
-#             except Exception as e:
-#                 print(f"Error during file processing: {e}")
-
-#         time.sleep(1)
-
-
-# def gcode_server():
-#     print("GCode server started.")
-#     command = "python gcode_server.py --gcode_dir gcode_outputs"
-
-#     subprocess.run(command, shell=True)
-
-#     # Wait for the subprocess to finish
-#     print("GCode server finished.")
-#     time.sleep(1)
-
-
-# if __name__ == "__main__":
-#     # Confirm that the output directory exists
-#     if not UPLOAD_DIR.exists():
-#         UPLOAD_DIR.mkdir()
-
-#     if not PROCESSING_DIR.exists():
-#         PROCESSING_DIR.mkdir()
-
-#     if not OUTPUT_DIR.exists():
-#         OUTPUT_DIR.mkdir()
-
-#     # Start the processing thread
-#     processing_thread = threading.Thread(target=process_files)
-#     processing_thread.start()
-
-#     # Start the GCode server thread
-#     # gcode_thread = threading.Thread(target=gcode_server)
-#     # gcode_thread.start()
-
-#     run_gcode_server(OUTPUT_DIR, run_flask=False)
-
-#     app.run("0.0.0.0", port=5010, use_reloader=False, debug=False)
