25b2d208146547c23c1d9b93b39d0118a7fe7c4b
Swapnil Pande <swapnil.pande98@gmail.com>
2024-08-30 15:27:56 -0400

    Eraser is erasing and communicating with main driver

--- etch-a-sketch-eraser/src/main.cpp
+++ etch-a-sketch-eraser/src/main.cpp
@@ -1,4 +1,8 @@
 #include <Arduino.h>
+#include <ArduinoOTA.h>
+#include <ESPmDNS.h>
+#include <WiFi.h>
+#include <WiFiUdp.h>
 #include <motorgo_mini.h>
 
 #include "common/lowpass_filter.h"
@@ -6,10 +10,14 @@
 #include "configurable.h"
 #include "pid_manager.h"
 
-#define FLIPPING_VELOCITY_RAD_PER_SEC 1.5
+// Intentionally crash compiler
+code breaks const char* ssid = "..............";
+const char* password = "..............";
+
+#define FLIPPING_VELOCITY_RAD_PER_SEC 2.0
 
 MotorGo::MotorGoMini motorgo_mini;
-MotorGo::MotorChannel& motor_right = motorgo_mini.ch0;
+MotorGo::MotorChannel& tilt_motor = motorgo_mini.ch0;
 
 MotorGo::MotorConfiguration gm3506_config;
 MotorGo::ChannelConfiguration config_ch0;
@@ -38,13 +46,13 @@ void enable_motors_callback(bool value)
   {
     Serial.println("Enabling motors");
     // motor_left.enable();
-    motor_right.enable();
+    tilt_motor.enable();
   }
   else
   {
     Serial.println("Disabling motors");
     // motor_left.disable();
-    motor_right.disable();
+    tilt_motor.disable();
   }
 }
 
@@ -60,36 +68,102 @@ void freq_println(String str, int freq)
   }
 }
 
+// blue -> 41
+// yellow -> 42
+uint8_t input_pin = 41;
+uint8_t output_pin = 42;
+bool erase_begin = false;
 void setup()
 {
   Serial.begin(5000000);
+
+  delay(5000);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.setHostname("etchbot-1-eraser");
+  WiFi.begin(ssid, password);
+
+  //   Try connecting 5 times, then give up
+  int connect_attempts = 0;
+  while (WiFi.status() != WL_CONNECTED && connect_attempts < 5)
+  {
+    delay(1000);
+    Serial.print(".");
+    connect_attempts++;
+  }
+
+  ArduinoOTA
+      .onStart(
+          []()
+          {
+            String type;
+            if (ArduinoOTA.getCommand() == U_FLASH)
+              type = "sketch";
+            else  // U_SPIFFS
+              type = "filesystem";
+
+            // NOTE: if updating SPIFFS this would be the place to unmount
+            // SPIFFS using SPIFFS.end()
+            Serial.println("Start updating " + type);
+          })
+      .onEnd([]() { Serial.println("\nEnd"); })
+      .onProgress(
+          [](unsigned int progress, unsigned int total)
+          { Serial.printf("Progress: %u%%\r", (progress / (total / 100))); })
+      .onError(
+          [](ota_error_t error)
+          {
+            Serial.printf("Error[%u]: ", error);
+            if (error == OTA_AUTH_ERROR)
+              Serial.println("Auth Failed");
+            else if (error == OTA_BEGIN_ERROR)
+              Serial.println("Begin Failed");
+            else if (error == OTA_CONNECT_ERROR)
+              Serial.println("Connect Failed");
+            else if (error == OTA_RECEIVE_ERROR)
+              Serial.println("Receive Failed");
+            else if (error == OTA_END_ERROR)
+              Serial.println("End Failed");
+          });
+
+  ArduinoOTA.setHostname("etchbot-1-eraser");
+
+  ArduinoOTA.begin();
+
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  pinMode(input_pin, INPUT);
+  pinMode(output_pin, OUTPUT);
+
+  digitalWrite(42, LOW);
 
   gm3506_config.pole_pairs = 11;
   gm3506_config.phase_resistance = 5.6;
   gm3506_config.calibration_voltage = 1.0;
 
   config_ch1.motor_config = gm3506_config;
-  config_ch1.power_supply_voltage = 6.0;
+  config_ch1.power_supply_voltage = 9.0;
   config_ch1.reversed = true;
 
-  motor_right.init(config_ch1, false);
+  tilt_motor.init(config_ch1, false);
 
   velocity_controller_parameters_ch1.p = 1.0;
   velocity_controller_parameters_ch1.lpf_time_constant = 0.1;
 
   // Configure the position controller
   position_controller.P = 3.5;
-  position_controller.I = 0.1;
+  position_controller.I = 0.5;
 
   //   We do this ONLY to update the internal low-pass filter for the position
   //   output
   position_controller_parameters.lpf_time_constant = 0.1;
-  motor_right.set_position_controller(position_controller_parameters);
-
-  motor_right.set_velocity_controller(velocity_controller_parameters_ch1);
-  motor_right.set_control_mode(MotorGo::ControlMode::Velocity);
-  motor_right.set_velocity_limit(FLIPPING_VELOCITY_RAD_PER_SEC);
-  motor_right.zero_position();
+  tilt_motor.set_position_controller(position_controller_parameters);
+  tilt_motor.set_velocity_controller(velocity_controller_parameters_ch1);
+  tilt_motor.set_control_mode(MotorGo::ControlMode::Velocity);
+  tilt_motor.set_velocity_limit(FLIPPING_VELOCITY_RAD_PER_SEC);
+  tilt_motor.zero_position();
 
   enable_motors.set_post_callback(enable_motors_callback);
 
@@ -109,95 +183,101 @@ void setup()
   ledcAttachPin(CH1_UL, 6);
   ledcAttachPin(CH1_VL, 7);
 
-  motor_right.enable();
+  tilt_motor.enable();
 }
 
 unsigned long last_time = micros();
-unsigned long hold_time = 5 * 1e6;
+unsigned long hold_time = 2 * 1e6;
 
 float zero_position = 0.0;
-float erase_position_1 = 1.62;
-float erase_position_2 = 0.9;
+float erase_position_1 = 1.10;
+float erase_position_2 = 3.25;
 float target_pos = erase_position_1;
 
 int cur_cycle_count = 0;
-int erase_cycles = 3;
+int erase_cycles = 5;
 
 bool current_pos = false;
 int erase_motor_pwm = 0.0;
+
+bool zeroing = false;
+unsigned long zero_start_time = 0;
+unsigned long zero_hold_time = 5 * 1e6;
 void loop()
 {
-  float error = target_pos - motor_right.get_position();
-  float output = position_controller(error);
-  motor_right.set_target_velocity(output);
-  // Update the motors
-  motor_right.loop();
+  ArduinoOTA.handle();
 
-  erase_motor_pwm = 255 * (2.0 / config_ch1.power_supply_voltage);
-
-  digitalWrite(CH1_UH, HIGH);
-  ledcWrite(7, erase_motor_pwm);
-
-  if (cur_cycle_count >= erase_cycles)
+  //   Wait until we receive a high on the input pin
+  if (digitalRead(input_pin) == HIGH)
   {
-    Serial.println("Erasing complete");
-
-    // Stop motors
-    motor_right.disable();
-
-    ledcWrite(7, 0);
-
-    while (true)
-    {
-      delay(1000);
-    }
+    digitalWrite(output_pin, HIGH);
+    erase_begin = true;
   }
 
-  if (micros() - last_time > hold_time)
+  if (erase_begin)
   {
-    if (current_pos)
-    {
-      target_pos = erase_position_1;
-      current_pos = false;
-    }
-    else
-    {
-      target_pos = erase_position_2;
-      current_pos = true;
-      cur_cycle_count++;
+    float error = target_pos - tilt_motor.get_position();
+    float output = position_controller(error);
+    tilt_motor.set_target_velocity(output);
+    // Update the motors
+    tilt_motor.loop();
 
-      Serial.println("Cycle count: " + String(cur_cycle_count));
+    erase_motor_pwm = 255 * (1.5 / config_ch1.power_supply_voltage);
+
+    digitalWrite(CH1_UH, HIGH);
+    ledcWrite(7, erase_motor_pwm);
+
+    if (cur_cycle_count >= erase_cycles)
+    {
+      if (!zeroing)
+      {
+        zeroing = true;
+        zero_start_time = micros();
+        target_pos = 0.0;
+        position_controller.reset();
+      }
+
+      if (micros() - zero_start_time > zero_hold_time)
+      {
+        tilt_motor.zero_position();
+        zeroing = false;
+        erase_begin = false;
+        cur_cycle_count = 0;
+
+        Serial.println("Erasing complete");
+
+        // Stop motors
+        tilt_motor.disable();
+
+        ledcWrite(7, 0);
+
+        // Set output to low to indicate that we are done erasing
+        digitalWrite(output_pin, LOW);
+
+        delay(1000);
+
+        // Restart the board
+        ESP.restart();
+      }
     }
 
-    last_time = micros();
+    if (micros() - last_time > hold_time && !zeroing)
+    {
+      if (current_pos)
+      {
+        target_pos = erase_position_1;
+        current_pos = false;
+      }
+      else
+      {
+        target_pos = erase_position_2;
+        current_pos = true;
+        cur_cycle_count++;
+
+        Serial.println("Cycle count: " + String(cur_cycle_count));
+      }
+
+      last_time = micros();
+    }
   }
 }
-
-// #include <Arduino.h>
-
-// void setup()
-// {
-//   Serial.begin(5000000);
-
-//   pinMode(CH1_UH, OUTPUT);
-//   pinMode(CH1_UL, OUTPUT);
-//   pinMode(CH1_VH, OUTPUT);
-//   pinMode(CH1_VL, OUTPUT);
-
-//   digitalWrite(CH1_UH, LOW);
-//   digitalWrite(CH1_UL, LOW);
-//   digitalWrite(CH1_VH, LOW);
-//   digitalWrite(CH1_VL, LOW);
-
-//   ledcSetup(6, 20000, 8);
-//   ledcSetup(7, 20000, 8);
-
-//   ledcAttachPin(CH1_UL, 6);
-//   ledcAttachPin(CH1_VL, 7);
-// }
-
-// void loop()
-// {
-//   digitalWrite(CH1_UH, HIGH);
-//   ledcWrite(7, 64);
-// }
