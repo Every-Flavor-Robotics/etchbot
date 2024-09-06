a67488592161ec030da9f2e017303727859ca892
Swapnil Pande <swapnil.pande98@gmail.com>
2024-09-06 14:30:26 -0400

    Switched to using absolute position

--- etch-a-sketch-eraser/src/main.cpp
+++ etch-a-sketch-eraser/src/main.cpp
@@ -11,8 +11,9 @@
 #include "pid_manager.h"
 
 // Intentionally crash compiler
-code breaks const char* ssid = "..............";
-const char* password = "..............";
+code breaks;
+const char* ssid = "............";
+const char* password = "............";
 
 #define FLIPPING_VELOCITY_RAD_PER_SEC 2.0
 
@@ -28,17 +29,34 @@ MotorGo::PIDParameters velocity_controller_parameters_ch0;
 MotorGo::PIDParameters velocity_controller_parameters_ch1;
 
 // Position controller parameters
-MotorGo::PIDParameters position_controller_parameters;
-PIDController position_controller(position_controller_parameters.p,
-                                  position_controller_parameters.i,
-                                  position_controller_parameters.d,
-                                  position_controller_parameters.output_ramp,
-                                  position_controller_parameters.limit);
+MotorGo::PIDParameters erasing_position_controller_parameters;
+MotorGo::PIDParameters holding_position_controller_parameters;
+PIDController position_controller(0.0, 0.0, 0.0, 0.0, 0.0);
 
 // configure wifi communications
 bool motors_enabled = false;
 ESPWifiConfig::Configurable<bool> enable_motors(motors_enabled, "/enable",
                                                 "Enable motors");
+
+unsigned long last_time = micros();
+unsigned long hold_time = 2.5 * 1e6;
+
+float zero_position = 0.06;
+float erase_position_1 = PI;
+float erase_position_2 = PI / 3;
+float target_pos = 0;
+
+int cur_cycle_count = 0;
+int erase_cycles = 5;
+
+bool current_pos = true;
+int erase_motor_pwm = 0.0;
+
+bool zeroing = false;
+unsigned long zero_start_time = 0;
+unsigned long zero_hold_time = 8 * 1e6;
+
+float error_tolerance = 0.1;
 
 void enable_motors_callback(bool value)
 {
@@ -66,6 +84,48 @@ void freq_println(String str, int freq)
     Serial.println(str);
     last_print_time = now;
   }
+}
+
+/// @brief  Return angle in range [0, 2pi)
+/// @param angle Angle in radians
+/// @return Angle in range [0, 2pi)
+float get_angle_absolute(float angle)
+{
+  // Return an angle between 0 and 2pi
+  while (angle < 0)
+  {
+    angle += 2 * PI;
+  }
+  //   Not inclusive of 2pi
+  while (angle >= 2 * PI)
+  {
+    angle -= 2 * PI;
+  }
+  return angle;
+}
+
+/// @brief  Get a target angle that is in the correct direction from the current
+/// @param cur_angle Current angle
+/// @param target_angle Target angle in range [0, 2pi)
+/// @param direction Direction to move in
+/// @return Target angle
+float get_target(float cur_angle, float target_angle, int direction)
+{
+  if (direction == 1)
+  {
+    while (target_angle < cur_angle)
+    {
+      target_angle += 2 * PI;
+    }
+  }
+  else
+  {
+    while (target_angle > cur_angle)
+    {
+      target_angle -= 2 * PI;
+    }
+  }
+  return target_angle;
 }
 
 // blue -> 41
@@ -141,10 +201,10 @@ void setup()
 
   gm3506_config.pole_pairs = 11;
   gm3506_config.phase_resistance = 5.6;
-  gm3506_config.calibration_voltage = 1.0;
+  gm3506_config.calibration_voltage = 2.0;
 
   config_ch1.motor_config = gm3506_config;
-  config_ch1.power_supply_voltage = 9.0;
+  config_ch1.power_supply_voltage = 15.0;
   config_ch1.reversed = true;
 
   tilt_motor.init(config_ch1, false);
@@ -152,18 +212,29 @@ void setup()
   velocity_controller_parameters_ch1.p = 1.0;
   velocity_controller_parameters_ch1.lpf_time_constant = 0.1;
 
+  holding_position_controller_parameters.p = 2.0;
+  holding_position_controller_parameters.i = 0.05;
+  holding_position_controller_parameters.limit = 2.0;
+
   // Configure the position controller
-  position_controller.P = 3.5;
-  position_controller.I = 0.5;
+  erasing_position_controller_parameters.p = 3.5;
+  erasing_position_controller_parameters.d = 0.5;
+
+  //   Set parameters for holding mode
+  position_controller.P = holding_position_controller_parameters.p;
+  position_controller.I = holding_position_controller_parameters.i;
+  position_controller.D = holding_position_controller_parameters.d;
+  position_controller.output_ramp =
+      holding_position_controller_parameters.output_ramp;
+  position_controller.limit = holding_position_controller_parameters.limit;
 
   //   We do this ONLY to update the internal low-pass filter for the position
   //   output
-  position_controller_parameters.lpf_time_constant = 0.1;
-  tilt_motor.set_position_controller(position_controller_parameters);
+  erasing_position_controller_parameters.lpf_time_constant = 0.1;
+  tilt_motor.set_position_controller(erasing_position_controller_parameters);
   tilt_motor.set_velocity_controller(velocity_controller_parameters_ch1);
   tilt_motor.set_control_mode(MotorGo::ControlMode::Velocity);
   tilt_motor.set_velocity_limit(FLIPPING_VELOCITY_RAD_PER_SEC);
-  tilt_motor.zero_position();
 
   enable_motors.set_post_callback(enable_motors_callback);
 
@@ -184,44 +255,51 @@ void setup()
   ledcAttachPin(CH1_VL, 7);
 
   tilt_motor.enable();
+  tilt_motor.loop();
+
+  //   Find the shortest distance to zero position, for holding
+  float target_forward =
+      get_target(tilt_motor.get_position(), zero_position, 1);
+  float target_backward =
+      get_target(tilt_motor.get_position(), zero_position, -1);
+
+  if (abs(target_forward) < abs(target_backward))
+  {
+    target_pos = target_forward;
+  }
+  else
+  {
+    target_pos = target_backward;
+  }
+
+  Serial.println(target_pos);
 }
 
-unsigned long last_time = micros();
-unsigned long hold_time = 2 * 1e6;
-
-float zero_position = 0.0;
-float erase_position_1 = 1.10;
-float erase_position_2 = 3.25;
-float target_pos = erase_position_1;
-
-int cur_cycle_count = 0;
-int erase_cycles = 5;
-
-bool current_pos = false;
-int erase_motor_pwm = 0.0;
-
-bool zeroing = false;
-unsigned long zero_start_time = 0;
-unsigned long zero_hold_time = 5 * 1e6;
 void loop()
 {
   ArduinoOTA.handle();
+  float angle = tilt_motor.get_position();
 
-  //   Wait until we receive a high on the input pin
-  if (digitalRead(input_pin) == HIGH)
+  // Wait until we receive a high on the input pin
+  if (digitalRead(input_pin) == HIGH && !erase_begin)
   {
     digitalWrite(output_pin, HIGH);
+
+    // Switch to erasing parameters
+    position_controller.P = erasing_position_controller_parameters.p;
+    position_controller.I = erasing_position_controller_parameters.i;
+    position_controller.D = erasing_position_controller_parameters.d;
+    position_controller.output_ramp =
+        erasing_position_controller_parameters.output_ramp;
+    position_controller.limit = erasing_position_controller_parameters.limit;
+
+    position_controller.reset();
+
     erase_begin = true;
   }
 
   if (erase_begin)
   {
-    float error = target_pos - tilt_motor.get_position();
-    float output = position_controller(error);
-    tilt_motor.set_target_velocity(output);
-    // Update the motors
-    tilt_motor.loop();
-
     erase_motor_pwm = 255 * (1.5 / config_ch1.power_supply_voltage);
 
     digitalWrite(CH1_UH, HIGH);
@@ -239,7 +317,6 @@ void loop()
 
       if (micros() - zero_start_time > zero_hold_time)
       {
-        tilt_motor.zero_position();
         zeroing = false;
         erase_begin = false;
         cur_cycle_count = 0;
@@ -265,12 +342,12 @@ void loop()
     {
       if (current_pos)
       {
-        target_pos = erase_position_1;
+        target_pos = get_target(angle, erase_position_1, 1);
         current_pos = false;
       }
       else
       {
-        target_pos = erase_position_2;
+        target_pos = get_target(angle, erase_position_2, -1);
         current_pos = true;
         cur_cycle_count++;
 
@@ -280,4 +357,18 @@ void loop()
       last_time = micros();
     }
   }
+  else
+  {
+    digitalWrite(CH1_UH, LOW);
+    ledcWrite(7, 0);
+  }
+
+  float error = target_pos - angle;
+
+  float output = position_controller(error);
+  // Print position and error
+  tilt_motor.set_target_velocity(output);
+
+  // Update the motors
+  tilt_motor.loop();
 }
