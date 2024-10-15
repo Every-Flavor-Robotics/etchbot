93b174cf1e3f439d65c6343f99a2fd48e2db168f
Swapnil Pande <swapnil.pande98@gmail.com>
2024-10-15 14:18:20 -0400

    Tuned KV for motors

--- etch-a-sketch-driver/src/motor_definitions.h
+++ etch-a-sketch-driver/src/motor_definitions.h
@@ -41,7 +41,7 @@ const MotorGo::MotorConfiguration GARTTLeftEtch1(11,        // pole_pairs
 );
 
 const MotorGo::MotorConfiguration GARTTRightEtch1(11,        // pole_pairs
-                                                  400,       // kv
+                                                  390,       // kv
                                                   0.1088f,   // phase_resistance
                                                   NOT_SET,   // phase_inductance
                                                   4.5,       // voltage_limit
